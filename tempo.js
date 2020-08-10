f3754ee6f5c7bc68b96a4d3178d60182b8425f1a
SampathReddy12345 <sampathreddy139@gmail.com>
2020-08-10 18:59:52 +0530

    modified tempo.js

--- tempo.js
+++ tempo.js
@@ -1,57 +1,49 @@
-function sudokuSolver(puzzle){
-    var nonPossibilities ={},impossibleNumbers,emptySpaces = 81;
-    while (emptySpaces > 0) {
-        emptySpaces = 0;
-          for (var vert = 0;vert < puzzle.length;vert++) {
-            for (var horz = 0;horz < puzzle.length;horz++) {                    
-                if(puzzle[vert][horz] === 0) {
-                    nonPossibilities ={};
-                    for( var i=0; i<9; i++) {
-                        if (puzzle[vert][i] > 0) {
-                            nonPossibilities[puzzle[vert][i]] = true;
-                        }
-                        if (puzzle[i][horz] > 0) {
-                            nonPossibilities[puzzle[i][horz]] =true; 
-                        }
-                    }
-                    for(var vertBox =Math.floor(vert / 3)*3;vertBox <Math.floor(vert /3) *3 +3;vertBox++) {
-                        for(var horzBox =Math.floor(vert / 3)*3;horzBox <Math.floor(horz /3) *3 +3;horzBox++) {
-                            if(puzzle[vertBox][horzBox]) {
-                                nonPossibilities[puzzle[vertBox][horzBox]] =true;
-                            }
-                        }
-                    }
-                    impossibleNumbers =Object.keys(nonPossibilities);
-                    if (impossibleNumbers.length === 8 ) {
-                        for(var i=1;i<10;i++) {
-                            if(impossibleNumbers.indexOf(i.toString()) <0) {
-                                console.log(vert,horz);
-                                puzzle[vert][horz] =i;
-                            }
-                        }    
-                    } 
-                    else {
-                        emptySpaces++;
-                    }
-                    
-                } 
-    
-            }
+// Solving using backtracking algorithm
+// Using puzzle from sudoku_puzzels.txt
+const _board = [
+    ['.', '.', '.', '2', '6', '.', '7', '.', '1'],
+    ['6', '8', '.', '.', '7', '.', '.', '9', '.'],
+    ['1', '9', '.', '.', '.', '4', '5', '.', '.'],
+    ['8', '2', '.', '1', '.', '.', '.', '4', '.'],
+    ['.', '.', '4', '6', '.', '2', '9', '.', '.'],
+    ['.', '5', '.', '.', '.', '3', '.', '2', '8'],
+    ['.', '.', '9', '3', '.', '.', '.', '7', '4'],
+    ['.', '4', '.', '.', '5', '.', '.', '3', '6'],
+    ['7', '.', '3', '.', '1', '8', '.', '.', '.'],
+];
+sodokoSolver(_board);
+console.log(_board);
+
+function isValid(board, row, col, k) {
+    for (let i = 0; i < 9; i++) {
+        const m = 3 * Math.floor(row / 3) + Math.floor(i / 3);
+        const n = 3 * Math.floor(col / 3) + i % 3;
+        if (board[row][i] == k || board[i][col] == k || board[m][n] == k) {
+          return false;
         }
     }
-        return puzzle;
-    }
+    return true;
+}
 
-var puzzle = [         
-    [5,3,0,0,7,0,0,0,0],
-    [6,0,0,1,9,5,0,0,0],
-    [0,9,8,0,0,0,0,6,0],
-    [8,0,0,0,6,0,0,0,3],
-    [4,0,0,8,0,3,0,0,1],
-    [7,0,0,0,2,0,0,0,6],
-    [0,6,0,0,0,0,2,8,0],
-    [0,0,0,4,1,9,0,0,5],
-    [0,0,0,0,8,0,0,7,9]]; 
 
-    
-console.log(sudokuSolver(puzzle));
+function sodokoSolver(data) {
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      if (data[i][j] == '.') {
+        for (let k = 1; k <= 9; k++) {
+          if (isValid(data, i, j, k)) {
+            data[i][j] = `${k}`;
+          if (sodokoSolver(data)) {
+           return true;
+          } else {
+           data[i][j] = '.';
+          }
+         }
+       }
+       return false;
+     }
+   }
+ }
+ return true;
+}
+// running the code matches the answer in sudoku_puzzles.txt
